428beb05ec6096a5e64c37d946aa2c6959abb75f
An Pham <anpham@cttdvnatl.org>
2022-09-03 09:52:43 -0400

    New Round Created

---
+++ src/pages/Round.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Round = () => {
+    return (
+        <div className='round'>
+            <div className='container'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Round;
---
+++ src/pages/Selection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Selection = () => {
+    return (
+        <div className='selection'>
+            <div className='container'>
+                <div className='select-round'>
+                    <Link className="link" to="/round/1">Phần I</Link>
+                    <Link className="link" to="/round/2">Phần II</Link>
+                    <Link className="link" to="/round/3">Phần III</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Selection;
